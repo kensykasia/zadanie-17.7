59e1cbf5434bff9dcbe3617d029ebc6d8e777f57
Katarzyna Kensy <zbierek@gmail.com>
2018-08-04 23:02:51 +0200

    Add all files

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+'GOOGLE_CLIENT_ID': '636294663734-hl75k6fenuovk34q30e1pg9bvc16gmdd.apps.googleusercontent.com',
+'GOOGLE_CLIENT_SECRET': 'BL9BdNREIJqExaJhO0AlF-f8',
+'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+}
